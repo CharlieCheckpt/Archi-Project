27640713df2ffcbf840e6794c7604a184ce66245
Philippe <philippe@philux>
2015-01-11 11:48:36 +0100

    continue

--- projet_archi.c
+++ projet_archi.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define T_MAX 256
 
+#define T_MAX 256
+#define INPUT_FILE "assembleur_test.txt"
+
+//Label
+typedef struct{
+	char label[T_MAX];
+} Label;
+
+//Intructions to int
 int instructions(char* str){
 	int i;
 	if (!strcmp(str, "push")) {
@@ -42,9 +50,10 @@ int instructions(char* str){
 	return i;
 }
 
+//Argument type
 int argtype(char* str){
 	/*
-		0 = pas d'arg
+		0 = pas d'argument
 		1 = un nombre
 		2 = une chaîne
 	 */
@@ -79,37 +88,45 @@ int argtype(char* str){
 		i=1;
 	}else if (!strcmp(str, "halt")){
 		i=0;
+	}else{
+		printf("Invalid argument");
+		exit(1);
 	}
 	return i;
 }
 
-typedef struct{
-	char label[T_MAX];
-} Label;
-
-typedef struct{
-	char Label[T_MAX];
-	char inst[T_MAX];
-	char arg[T_MAX];
-} Line;
-
+//Lines counter
 int countLines(FILE* file){
 	int line_number = 0;
 	char str[T_MAX];
 	if (file!=NULL){
 		while(fgets(str,T_MAX,file)!='\0') {
-			printf("line nb = %d\n", line_number);
+			//printf("line nb = %d\n", line_number);
 			line_number++;
 		}
 	}
 	return line_number;
 }
 
-Label* createLabelTab(FILE* file, int line_count){
+//Label table creator
+Label* createLabelTab(int line_count){
 	Label* label_tab = malloc(sizeof(Label)*line_count);
 	return label_tab;
 }
 
+//Label table filling
+/*
+	fgets(str,T_MAX,file) lit T_MAX-1 caractères de fichier file et le met dans str
+
+	strstr(str, ":") renvoie un pointeur dans str qui pointe vers une chaine commencant par ":"
+		char str[T_MAX] = "Mon chat est blanc"
+		strstr(str, "chat") renvoie "chat est blanc" (pointeur vers le premier char)
+		strstr(str, "mignon") renvoie NULL
+
+	strtok(str, " \t\r\n:") renvoie un pointeur dans str vers une chaine finissant par un espace, tab, CR ou LF suivi de ":"
+		char str[T_MAX] = "Je suis Charlie"
+		mot = strtok(str, "suis") met "Je " dans mot
+ */
 void fillLabelTab(FILE* file, Label* label_tab){
 	char str[T_MAX];
 	char *word;
@@ -121,16 +138,20 @@ void fillLabelTab(FILE* file, Label* label_tab){
 				word = strtok(str, " \t\r\n:");
 				printf("mot (%d) = %s\n", i, word);
 
+				label_tab[i].label = "hi";
 			}
 		}
 	}
 }
 
+//Find a label in the table and returns the line #
+/*
+	On ecrit sizeof(label_tab)/ sizeof(Label) car sizeof(label_tab) renvoie la taille du tableau en bits (et non en nombre de "cases")
+ */
 int findLabel(char* str, Label* label_tab){
 	int i;
 	printf("%d", sizeof(label_tab));
 	for(i=0; i< sizeof(label_tab)/ sizeof(Label); i++){
-
 		if(strcmp(label_tab[i].label, str) == 0)
 			return i;
 	}
@@ -138,36 +159,46 @@ int findLabel(char* str, Label* label_tab){
 }
 
 int main(){
-    FILE*file=NULL;
-	FILE*file2=NULL;
-    file=fopen("assembleur_test.txt","r+");
-	int cl = countLines(file);
+    FILE* file=NULL;
+	FILE* file2=NULL;
 
-	Label* label_tab = createLabelTab(file, cl);
+    file=fopen(INPUT_FILE,"r+");
 
+	//totalline = nombre de lignes
+	int totalline = countLines(file);
+
+	//creation du tableau
+	Label* label_tab = createLabelTab(totalline);
 	fclose(file);
-	file=fopen("assembleur_test.txt","r+");
 
+	//remplissage du tableau des etiquettes
+	file=fopen(INPUT_FILE,"r+");
 	fillLabelTab(file, label_tab);
-
 	fclose(file);
+
+	int i;
+	for(i=0; i< 5; i++){
+		printf("Dans la case %d, il y a letiquette %s\n", i, label_tab[i].label);
+	}
 	
-	file=fopen("assembleur_test.txt","r+");
+	file=fopen(INPUT_FILE,"r+");
+
+	//creation fichier d'exportation
 	file2=fopen("out.txt","w");
 	
 	char str[T_MAX];
-	char *word;
-	char *arg;
-	char *useless;
-	int argint;
+	char *word; //mot a transformer en nombre avec la fonction instrucstions
+	char *arg; //argument de type chaine de caracteres
+	char *useless; //pour ignorer les etiquettes lors de la traduction
+	int argint; //argument de type entier
 	int line_n = 0;
+
 	if (file!=NULL){
 		while(fgets(str,T_MAX,file)) {
 			line_n++;
-			//fscanf(file, "%s", &word);
 
 			if (strstr(str, ":") != NULL) {
-				useless = strtok(str, " \t\r\n:");
+				useless = strtok(str, " \t\r\n:"); //met l'etiquette de cote
 				word = strtok(NULL, " \t\r\n:");
 			} else {
 				word = strtok(str, " \t\r\n:");
@@ -195,7 +226,7 @@ int main(){
 			printf("argu = %s\n", arg);
 			//fonction qui transforme str en int s'il le faut
 			printf("code = %02X + arg = %08X \n\n",instructions(word), argint);
-			fprintf(file2,"%02X%08X \n\n",instructions(word), argint);
+			//fprintf(file2,"%02X%08X \n\n",instructions(word), argint);
 		}
 	}
 	
@@ -203,6 +234,3 @@ int main(){
     return 0;
 
 }
-
-
-//test2
